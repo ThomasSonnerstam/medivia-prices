f6e8c323a01a1ed06d73d756df20fac3e3ea36ca
Thomas Sönnerstam <50919150+ThomasSonnerstam@users.noreply.github.com>
2020-09-28 07:51:29 +0200

    Added ziyad and draculd

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,6 @@ const App = () => {
     arak: [
       { name: "Halberd", price: 400 },
       { name: "Brass armor", price: 150 },
-      { name: "Morning star", price: 90 },
       { name: "Battle hammer", price: 120 },
       { name: "Plate armor", price: 400 },
       { name: "Steel shield", price: 80 },
@@ -23,7 +22,8 @@ const App = () => {
     ],
     osaris: [
       { name: "Double axe", price: 260 },
-      { name: "Steel helmet", price: 293 }
+      { name: "Steel helmet", price: 293 },
+      { name: "Morning star", price: 100 },
     ],
     garrogat: [
       { name: "Clerical mace", price: 170 },
@@ -38,8 +38,8 @@ const App = () => {
     bluedjinn: [
       { name: "Dragon lance", price: 9000 },
       { name: "Fire axe", price: 8000 },
-      { name: "Fire sword", price: 400 },
-      { name: "War hammer", price: 4000 },
+      { name: "Fire sword", price: 4000 },
+      { name: "War hammer", price: 1200 },
       { name: "Spike sword", price: 1000 },
       { name: "Ice rapier", price: 1000 },
       { name: "Broad sword", price: 500 },
@@ -109,7 +109,6 @@ const App = () => {
       { name: "Crown boots", price: 12000 },
       { name: "Demonbone boots", price: 14000 },
       { name: "Light armor", price: 13000 },
-      { name: "Swinging demolisher", price: 11000 },
       { name: "Torn quicksand boots", price: 10000 },
       { name: "Demonbone helmet", price: 9000 },
       { name: "Fiery armor", price: 7500 },
@@ -138,6 +137,82 @@ const App = () => {
       { name: "Crystallized crossbow", price: 55000 },
       { name: "Envenomed crossbow", price: 65000 },
       { name: "Elven crossbow", price: 7500 },
+    ],
+    ziyad: [
+      { name: "Mercenary sword", price: 45000 },
+      { name: "Dragon slayer", price: 40000 },
+      { name: "Hellforged shield", price: 36000 },
+      { name: "Heavy mace", price: 35000 },
+      { name: "Heavy halberd", price: 35000 },
+      { name: "Crystal ring", price: 100 },
+      { name: "Steel boots", price: 30000 },
+      { name: "Dragon scale mail", price: 30000 },
+      { name: "Demon shield", price: 30000 },
+      { name: "Grim legs", price: 30000 },
+      { name: "Frozen shield", price: 29000 },
+      { name: "Bascinet", price: 24000 },
+      { name: "Imperial soldier legs", price: 20000 },
+      { name: "Arming sword", price: 20000 },
+      { name: "Golden armor", price: 20000 },
+      { name: "Ring of the sky", price: 20000 },
+      { name: "Grim boots", price: 10000 },
+      { name: "Swinging demolisher", price: 11000 },
+      { name: "Bright sword", price: 8000 },
+      { name: "Gold ring", price: 8000 },
+      { name: "Guardian halberd", price: 8000 },
+      { name: "Medusa shield", price: 8000 },
+      { name: "The stinger", price: 7000 },
+      { name: "Nether shield", price: 6600 },
+      { name: "Shakirian wand", price: 6000 },
+      { name: "Imperial soldier armor", price: 5500 },
+      { name: "Light sword", price: 4500 },
+      { name: "Wand of light", price: 4000 },
+      { name: "Imperial soldier shield", price: 3000 },
+      { name: "Platinum amulet", price: 1500 },
+      { name: "Scarab shield", price: 1500 },
+      { name: "Assassin dagger", price: 1000 },
+      { name: "Shakirian waraxe", price: 1000 },
+      { name: "Devil helmet", price: 1000 },
+      { name: "Mystic turban", price: 1000 },
+      { name: "Smoking pipe", price: 600 },
+      { name: "Ancient amulet", price: 500 },
+      { name: "Scarab amulet", price: 500 },
+      { name: "Aquamarine", price: 350 },
+      { name: "Bone shield", price: 100 },
+      { name: "Crystallized shield", price: 16000 },
+      { name: "Ruby necklace", price: 900 },
+    ],
+    draculd: [
+      { name: "Pirate sabre", price: 95 },
+      { name: "Worn out pirate tabard", price: 100 },
+      { name: "Red skirt", price: 150 },
+      { name: "Worn out pirate beater", price: 200 },
+      { name: "Cursed gold coin", price: 250 },
+      { name: "Worn out pirate cape", price: 250 },
+      { name: "Frost dagger", price: 800 },
+      { name: "Permafrost longsword", price: 900 },
+      { name: "Wendigo staff", price: 900 },
+      { name: "Alpha wolf tooth chain", price: 1000 },
+      { name: "Pirate eye patch", price: 1000 },
+      { name: "Pirate hand hook", price: 1500 },
+      { name: "Permafrost morningstar", price: 1600 },
+      { name: "Plaguespreader rod", price: 2000 },
+      { name: "Permafrost crystal", price: 3000 },
+      { name: "Pirate skull belt", price: 3000 },
+      { name: "Red tunic", price: 5000 },
+      { name: "Permafrost stone", price: 7000 },
+      { name: "Soul reaper", price: 7500 },
+      { name: "Crystallized sword", price: 10000 },
+      { name: "Crystallized axe", price: 11000 },
+      { name: "Crystallized hammer", price: 13000 },
+      { name: "Pirate bandana", price: 15000 },
+      { name: "Yellow robe", price: 18000 },
+      { name: "Golden staff", price: 20000 },
+      { name: "Fire hammer", price: 20000 },
+      { name: "Pirate hat", price: 20000 },
+      { name: "Permafrost robe", price: 26000 },
+      { name: "Plaguespreader robe", price: 26000 },
+      { name: "Lightbringer helmet", price: 40000 },
     ]
   }
 
@@ -155,6 +230,8 @@ const App = () => {
           <Button handleClick={() => { setSelected("greendjinn") }} city="Green djinn" />
           <Button handleClick={() => { setSelected("wulkan") }} city="Wulkan" />
           <Button handleClick={() => { setSelected("aremis") }} city="Aremis" />
+          <Button handleClick={() => { setSelected("ziyad") }} city="Ziyad" />
+          <Button handleClick={() => { setSelected("draculd") }} city="Draculd" />
         </ButtonContainer>
 
         <List>
@@ -247,6 +324,30 @@ const App = () => {
         })}
 
         {selected == "aremis" && location.aremis.map((item, i) => {
+          return (
+            <Fragment key={i}>
+              <div style={{ display: "flex", justifyContent: "space-between" }} >
+                <Item item={item.name} />
+                <Item item2={item.price} />
+              </div>
+              <div style={{ width: "100%", height: "1px", backgroundColor: "white"}}></div>
+            </Fragment>
+          )
+        })}
+
+        {selected == "ziyad" && location.ziyad.map((item, i) => {
+          return (
+            <Fragment key={i}>
+              <div style={{ display: "flex", justifyContent: "space-between" }} >
+                <Item item={item.name} />
+                <Item item2={item.price} />
+              </div>
+              <div style={{ width: "100%", height: "1px", backgroundColor: "white"}}></div>
+            </Fragment>
+          )
+        })}
+
+        {selected == "draculd" && location.draculd.map((item, i) => {
           return (
             <Fragment key={i}>
               <div style={{ display: "flex", justifyContent: "space-between" }} >
