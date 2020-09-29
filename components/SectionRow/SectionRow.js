6e85d39f38a9a4db9fbc0cd6aacfbe1f5b65501f
Thomas Sönnerstam <50919150+ThomasSonnerstam@users.noreply.github.com>
2020-09-29 07:38:38 +0200

    Other stuff

--- components/SectionRow/SectionRow.js
+++ components/SectionRow/SectionRow.js
@@ -2,14 +2,10 @@ import styled from "@emotion/styled";
 
 const SectionRow = styled.section`
   display: flex;
-  width: 100vw;
+  width: 100%;
   align-items: center;
   flex-wrap: wrap;
   justify-content: space-evenly;
-  padding: 2vh;
-
-  @media screen and (min-width: 700px) {
-  }
 `;
 
 export default SectionRow;
